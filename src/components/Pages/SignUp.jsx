ae376e184c4666cac9a1685554d99d178bc3b92f
Tarik Doganay <99910240+GoodGuyForReal@users.noreply.github.com>
2022-12-15 17:01:34 +0100

    signIn - signUp responsive update

--- src/components/Pages/SignUp.jsx
+++ src/components/Pages/SignUp.jsx
@@ -56,20 +56,20 @@ const SignUp = () => {
     return (
         <div className='h-[91vh] w-full'>
 
-            <div className='SignUpPage grid grid-cols-2 gap-4 items-center justify-center h-full'>
+            <div className='SignInPage md:flex gap-4 items-center justify-center h-full overflow-y-hidden'>
 
-                <div className='h-full w-full flex flex-col items-center justify-center ' >
-                    <div className='SignUpleftbody flex flex-col gap-10'>
+                <div className='signinEventContainer h-full w-full md:w-1/2 flex flex-col items-center justify-center ' >
+                    <div className='SignUpleftbody flex w-full max-w-[50vh] px-3 flex-col gap-10'>
                         <div className='text-left'>
                             <h1 className='text-black font-bold text-[42px] mb-2'>SignUp</h1>
                             <p className='text-[#0007] text-[16px]'>Welcome please enter your deatil</p>
                             {error && <p className='bg-[#ff314d] text-[15px] rounded-md mt-5 duration-300 ease-out text-white p-3'>{error}</p>}
                         </div>
                         <form className='flex flex-col gap-5 text-center'>
-                            <input onChange={(e) => setDisplayName(e.target.value)} type="text" placeholder='User Name' className='p-3 border rounded-md border-[#2227] w-[50vh] text-black' />
-                            <input onChange={(e) => setEmail(e.target.value.toLocaleLowerCase())} type="text" placeholder='Email' className='p-3 border rounded-md border-[#2227] w-[50vh] text-black' />
-                            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' className='p-3 border rounded-md border-[#2227] w-[50vh] text-black' />
-                            <input onChange={(e) => setVerify(e.target.value)} type="password" placeholder='Verify your password' className='p-3 border rounded-md border-[#2227] w-[50vh] text-black' />
+                            <input onChange={(e) => setDisplayName(e.target.value)} type="text" placeholder='User Name' className='p-3 border rounded-md border-[#2227] w-full text-black' />
+                            <input onChange={(e) => setEmail(e.target.value.toLocaleLowerCase())} type="text" placeholder='Email' className='p-3 border rounded-md border-[#2227] w-full text-black' />
+                            <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' className='p-3 border rounded-md border-[#2227] w-full text-black' />
+                            <input onChange={(e) => setVerify(e.target.value)} type="password" placeholder='Verify your password' className='p-3 border rounded-md border-[#2227] w-full text-black' />
                             <button onClick={HandleSubmit} className='bg-[#ff4fad] text-white py-3 hover:duration-300 hover:bg-[#ff74be]'>Sign Up</button>
                             <Link className='text-black underline hover:duration-300 hover:text-[#c1c1c1]' to={'/SignIn'}>Do you have an account</Link>
                         </form>
@@ -77,8 +77,9 @@ const SignUp = () => {
 
                 </div>
 
-                <div className='bg-white h-full w-full visible'>
-                    <img src={bannerImg} alt={bannerImg}  className='w-full h-full object-cover rounded-tl-2xl rounded-bl-2xl' />
+
+                <div className='signinImageContainer bg-white h-full md:w-1/2'>
+                    <img src={bannerImg} alt={bannerImg} className='w-full h-full object-cover rounded-tl-2xl rounded-bl-2xl' />
                 </div>
 
             </div>
